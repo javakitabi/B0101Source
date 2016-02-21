a8262ec3a22526f5e55bf180bf5e414b9624760d
javakitabi <mustafa@javakitabi.com>
2016-02-21 11:38:04 +0200

    Proje Üretildi

---
+++ Selam.java
@@ -0,0 +1,17 @@
+public class Selam{
+
+	private String world = "millet";
+
+	public String selamSoyle(String kime){
+		
+		String cumle;
+		
+		if(kime != "")
+			cumle = "Selam " + kime + " :)";
+		else
+			cumle = "Selam " + world + " :)";
+		return cumle;
+
+	}
+
+}
---
+++ SelamTest.java
@@ -0,0 +1,11 @@
+public class SelamTest{
+	
+	public static void main(String[] args){
+		
+		Selam selam = new Selam();
+		String cevap = selam.selamSoyle("Ali");
+		System.out.println(cevap);
+		
+	}
+		
+}
